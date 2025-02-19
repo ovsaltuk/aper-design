5905cc3587ae2904f8dbfc06c27bf13aa26fcb5b
ovsaltuk <116757654+ovsaltuk@users.noreply.github.com>
2025-02-19 19:05:36 +0300

    добавить баннер

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import "../../styles/banner.scss";
+
+export const Banner: FC = () => {
+  return (
+    <section className="banner">
+      <div className="banner__cube">
+        
+      </div>
+    </section>
+  );
+};
