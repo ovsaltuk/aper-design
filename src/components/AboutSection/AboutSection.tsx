f26c832c9748f2a6cd7ef8e6241d98c057a301c6
ovsaltuk <116757654+ovsaltuk@users.noreply.github.com>
2025-02-20 20:31:07 +0300

    добавить лого

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -2,7 +2,9 @@ import { FC } from "react";
 import "../../styles/aboutSection.scss";
 
 export const AboutSection: FC = () => {
-    return (<section className="about">
-        about
-    </section>)
-}
+  return (
+    <section className="about">
+      <div className="container">about</div>
+    </section>
+  );
+};
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,11 +1,19 @@
 import { FC } from "react";
+import logoSRC from "../../assets/logo.svg";
 import "../../styles/banner.scss";
 
 export const Banner: FC = () => {
   return (
     <section className="banner">
-      <div className="banner__cube">
-        
+      <div className="container">
+        <img src={logoSRC} alt="logo" className="logo" />
+        <div className="title-container">
+          <h1 className="title">Воплощение идей в жизнь</h1>
+          <span className="subtitle">
+            Авторский дизайн от Анастасии Первовой
+          </span>
+          <button className="button">Заказать дизайн</button>
+        </div>
       </div>
     </section>
   );
