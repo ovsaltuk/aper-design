1fb4c9a787fe971a1eddd3e04a81033fa941c8fb
ovsaltuk <116757654+ovsaltuk@users.noreply.github.com>
2025-02-27 22:29:19 +0300

    добавить фон к проекту фамилия

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -7,7 +7,7 @@ export const ProjectsSection: FC = () => {
       <div className="projects__container">
         <div className="projects__block">
           <div className="projects__item-title">
-            <span>Title</span>
+            <span>familia</span>
           </div>
         </div>
         <div className="projects__block">
